43c0108d88f47f9514f487626e2937a5794c3536
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-06-02 17:21:32 +0530

    create a copy for diff

--- 4. Crawling The Bitcoin Network/crawler_two.py
+++ 4. Crawling The Bitcoin Network/crawler_two.py
@@ -1,16 +1,6 @@
-import time
-
-from io import BytesIO
-
 from lib import handshake, read_msg, serialize_msg, read_varint, read_address, BitcoinProtocolError
-
-
-def read_addr_payload(stream):
-    r = {}
-    count = read_varint(stream)
-    r['addresses'] = [read_address(stream) for _ in range(count)]
-    return r
-
+from io import BytesIO
+import time
 
 class Node:
 
@@ -22,7 +12,6 @@ class Node:
     def address(self):
         return (self.ip, self.port)
 
-
 class Connection:
 
     def __init__(self, node):
@@ -31,7 +20,7 @@ class Connection:
         self.stream = None
         self.start = None
 
-        # Results
+        # results
         self.peer_version_payload = None
         self.nodes_discovered = []
 
@@ -39,46 +28,46 @@ class Connection:
         msg = read_msg(self.stream)
         command = msg['command']
         payload_len = len(msg['payload'])
-        print(f'Received a "{command}" containing {payload_len} bytes')
+        print('Received a {} containing {} bytes'.format(command, payload_len))
 
-        # Respond to "ping"
+        # respond to ping
         if command == b'ping':
             res = serialize_msg(command=b'pong', payload=msg['payload'])
             self.sock.sendall(res)
-            print("Send 'pong'")
+            print('Sent pong')
 
-        # Specially handle peer lists
+        # handle peer lists
         if command == b'addr':
             payload = read_addr_payload(BytesIO(msg['payload']))
             if len(payload['addresses']) > 1:
-                self.nodes_discovered = [
-                    Node(a['ip'], a['port']) for a in payload['addresses']
-                ]
+                self.nodes_discovered.extend([
+                    (a['ip'], a['port']) for a in payload['addresses']
+                    ])
+                break
 
     def remain_alive(self):
         return not self.nodes_discovered
 
     def open(self):
-        # Set start time
+        # set start time
         self.start = time.time()
 
-        # Open TCP connection
-        print(f'Connecting to {self.node.ip}')
-        self.sock = handshake(self.node.address)  # FIXME: save the version payload
+        # open TCP connection
+        print("Connecting to {}".format(self.node.ip))
+        self.sock = handshake(self.node.address)
         self.stream = self.sock.makefile('rb')
 
         # Request peer's peers
         self.sock.sendall(serialize_msg(b'getaddr'))
 
-        # Handle messages until program exists
+        # Handle messages until program exits
         while self.remain_alive():
             self.handle_msg()
 
     def close(self):
-        # Clean up socket's file descriptor
+        # clean up socket's file descriptor
         if self.sock:
             self.sock.close()
-
 
 class Crawler:
 
@@ -88,30 +77,34 @@ class Crawler:
     def crawl(self):
         pass
 
+def read_addr_payload(stream):
+    r = {}
+
+    # read varint
+    count = read_varint(stream)
+
+    # read_address varint times. Return as list.
+    r["addresses"] = [read_address(stream) for _ in range(count)]
+    return r
+
 def crawler(nodes):
     while True:
-        # Get next address from addresses and connect
         node = nodes.pop()
-
         try:
             conn = Connection(node)
             conn.open()
         except (OSError, BitcoinProtocolError) as e:
-            print(f'Got error: {str(e)}')
+            print("Got error {}".format(str(e)))
             continue
         finally:
             conn.close()
 
         # Handle the results
         nodes.extend(conn.nodes_discovered)
-        print(f'{conn.node.ip} report version {conn.peer_version_payload}')
+        print("{} reports version {}".format(conn.nodes.ip, conn.peer_version_payload))
+
 
 
 if __name__ == '__main__':
-    nodes = [
-        Node('92.109.124.73', 8333),
-        Node('67.205.160.50', 8333),     
-        Node('54.169.196.130', 8333),     
-    ]
-
+    nodes = [Node('204.236.245.12', '8333')]
     crawler(nodes)
