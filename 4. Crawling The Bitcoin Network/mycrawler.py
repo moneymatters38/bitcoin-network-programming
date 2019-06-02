7a61d9f6be0add227f6d2fa962b3ce527028e056
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-06-02 11:31:35 +0530

    read addr

--- 4. Crawling The Bitcoin Network/mycrawler.py
+++ 4. Crawling The Bitcoin Network/mycrawler.py
@@ -1,9 +1,23 @@
-from lib import handshake, read_msg, serialize_msg
+from lib import handshake, read_msg, serialize_msg, read_varint
+
+def read_addr_payload(stream):
+    r = {}
+
+    # read varint
+    count = read_varint(stream)
+
+    # read_address varint times. Return as list.
+    r["addresses"] = [read_address(stream) for _ in range(count)]
+    return r
+
 
 def listener(address):
     # Establish connection
     sock = handshake(address)
     stream = sock.makefile('rb')
+
+    # Request peer's peers
+    sock.sendall(serialize_msg(b'getaddr'))
 
     # Print every possible gossip message we receive
     while True:
@@ -17,6 +31,11 @@ def listener(address):
             res = serialize_msg(command=b'pong', payload=msg['payload'])
             sock.sendall(res)
             print('Sent pong')
+
+        # handle peer lists
+        if command == b'addr':
+            payload = read_addr_payload(BytesIO(msg['payload']))
+            print(msg['payload'])
 
 if __name__ == '__main__':
     listener(('204.236.245.12', '8333'))
