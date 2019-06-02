08b661be9360df6a18b7f42352662c96f2b03581
Deepak M. Kathayat <dmkathayat@gmail.com>
2019-06-02 16:50:12 +0530

    extend connection class

--- 4. Crawling The Bitcoin Network/mycrawler.py
+++ 4. Crawling The Bitcoin Network/mycrawler.py
@@ -13,11 +13,48 @@ class Node:
 
 class Connection:
 
-    def __init__(self):
-        pass
+    def __init__(self, node):
+        self.node = node
+        self.sock = None
+        self.stream = None
+        self.start = None
+
+        # results
+        self.peer_version_payload = None
+        self.nodes_discovered = []
 
     def open(self):
-        pass
+        # Establish connection
+        print("Connecting to {}".format(self.node.ip))
+        self.sock = handshake(self.node.address)
+        self.stream = self.sock.makefile('rb')
+
+        # Request peer's peers
+        self.sock.sendall(serialize_msg(b'getaddr'))
+
+        # Print every possible gossip message we receive
+        while True:
+            # Handle next message
+            msg = read_msg(self.stream)
+            command = msg['command']
+            payload_len = len(msg['payload'])
+            print('Received a {} containing {} bytes'.format(command, payload_len))
+
+            # respond to ping
+            if command == b'ping':
+                res = serialize_msg(command=b'pong', payload=msg['payload'])
+                self.sock.sendall(res)
+                print('Sent pong')
+
+            # handle peer lists
+            if command == b'addr':
+                payload = read_addr_payload(BytesIO(msg['payload']))
+                if len(payload['addresses']) > 1:
+                    self.nodes_discovered.extend([
+                        (a['ip'], a['port']) for a in payload['addresses']
+                        ])
+                    break
+
 
     def close(self):
         pass
@@ -40,43 +77,22 @@ def read_addr_payload(stream):
     r["addresses"] = [read_address(stream) for _ in range(count)]
     return r
 
-
-def listener(addresses):
+def crawler(nodes):
     while True:
-        address = addresses.pop()
+        node = nodes.pop()
         try:
-            # Establish connection
-            print("Connecting to {}".format(address))
-            sock = handshake(address)
-            stream = sock.makefile('rb')
-
-            # Request peer's peers
-            sock.sendall(serialize_msg(b'getaddr'))
-
-            # Print every possible gossip message we receive
-            while True:
-                msg = read_msg(stream)
-                command = msg['command']
-                payload_len = len(msg['payload'])
-                print('Received a {} containing {} bytes'.format(command, payload_len))
-
-                # respond to pong
-                if command == b'ping':
-                    res = serialize_msg(command=b'pong', payload=msg['payload'])
-                    sock.sendall(res)
-                    print('Sent pong')
-
-                # handle peer lists
-                if command == b'addr':
-                    payload = read_addr_payload(BytesIO(msg['payload']))
-                    if len(payload['addresses']) > 1:
-                        addresses.extend([
-                            (a['ip'], a['port']) for a in payload['addresses']
-                            ])
-                        break
+            conn = Connection(node)
+            conn.open()
         except Exception as e:
             print("Got error {}".format(str(e)))
             continue
 
+        # Handle the results
+        nodes.extend(conn.nodes_discovered)
+        print("{} reports version {}".format(conn.nodes.ip, conn.peer_version_payload))
+
+
+
 if __name__ == '__main__':
-    listener([('204.236.245.12', '8333')])
+    nodes = [Node('204.236.245.12', '8333')]
+    crawler(nodes)
